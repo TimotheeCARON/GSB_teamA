a550e6ac6d2342153d7c2370f091e4cea4ac849e
Timothée CARON <timoc6969@gmail.com>
2018-03-29 17:01:09 +0200

    MaJ c + v Visiteurs médicaux

--- GSB/controleurs/c_visiteurs.php
+++ GSB/controleurs/c_visiteurs.php
@@ -4,17 +4,33 @@ $idVisiteur = $_SESSION['idVisiteur'];
 $action = $_REQUEST['action'];
 
 switch($action){
-	/*case 'AFFICHEMedicaments':{
-		$lesMedicaments = $_REQUEST['AfficherlesMedocs'];
+	/*case 'AFFICHEVisiteurs':{
+		$lesVisiteurs = $_REQUEST['AfficherlesMedocs'];
 		
 	  	 	$pdo->AfficherMedocs($Numero,$Nom,$Famille,$Effet,$Presentation,$Dosage,$ContreIndication,$PrixHT,$PrixEchantillon);
 	}*/
-	case 'valideConnexion':{
 
+
+
+
+
+ 	case 'NewVisiteur':{
+	if(!empty($_POST)){
+		$nom=$_POST['nom'];
+		$prenom=$_POST['prenom'];
+		$adresse=$_POST['adresse'];
+		$cp=$_POST['cp'];
+		$ville=$_POST['ville'];
+		$dateEmbauche=$_POST['dateEmbauche'];
+		$idSecteur=$_POST['idSecteur'];
+		setVisiteur ($nom,$prenom,$adresse,$cp,$ville,$dateEmbauche,$idSecteur);
+		}
 	}
 }
 
+
+
 $lesVisiteurs= $pdo->getVisiteur();
-$lesFamillesMedicaments = $pdo->getVisite();
+$lesVisites= $pdo->getVisite();
 include("vues/v_visiteurs.php");
 ?>
--- GSB/vues/v_visiteurs.php
+++ GSB/vues/v_visiteurs.php
@@ -1,26 +1,18 @@
 <div id="contenu">
-    <h2>Enregistrer un nouveau médicament</h2>
-    <form method="POST" action="index.php?uc=medicaments&action=NewMedoc">
+    <h2>Enregistrer un nouveau visiteur médical</h2>
+    <form method="POST" action="index.php?uc=visiteurs&action=NewVisiteur">
         <input type="text" name="nom" />Nom du médicament<br />
         <input type="text" name="prenom" />Effet thérapeutique<br />
         <input type="text" name="adresse" />Contre indication<br />
-        <input type="text" name="" />Presentation<br />
-        <input type="text" name="Dosage" />Dosage<br />
-        <input type="text" name="pxHT" />pxHT<br />
-        <input type="text" name="pxEchantillon" />pxEchantillon<br />
-        <?php
-            
-        
-            foreach ($lesFamillesMedicaments as $uneFamille){
-                echo '<option value= '.$uneFamille['idFamille'].' > '.$uneFamille['nomFamille'].' </option>';
-            }
-            ?>
-            
-        </select> 
+        <input type="text" name="cp" />Presentation<br />
+        <input type="text" name="ville" />Dosage<br />
+        <input type="text" name="dateEmbauche" />pxHT<br />
+        <input type="text" name="idSecteur" />pxEchantillon<br />
 
         <input type="submit" value="Envoyer" />
     
     </form>
+
 </div>
 
 <div id="contenu">
@@ -28,34 +20,33 @@
          
       
   	<table>
-  	   <caption>Descriptif des éléments hors forfait -
+  	   <caption>Récapitulatif des visiteurs
            </caption>
              <tr>
-                <th class="id">Numero</th>
+                
                 <th class="nom">Nom</th>
-                <th class="famille">Prénom</th>  
-                <th class="effet">Adresse</th>
-                <th class="dosage">Code Postal</th>
-                <th class="presentation">Présentation</th>
-                <th class="contre_indication">Contre indiction</th>
-                <th class="prix_HT">Prix HT</th>
-                <th class="prix_echantillon">Prix échantillon</th>
+                <th class="prenom">Prénom</th>  
+                <th class="adresse">Adresse</th>
+                <th class="cp">Code Postal</th>
+                <th class="ville">Ville</th>
+                <th class="dateEmbauche">Date d'embauche</th>
+                <th class="idSecteur">Secteur HT</th>
                 
              </tr>
               <?php      
           foreach ( $lesVisiteurs as $unVisiteur ) 
 		  {
-			$id = $unVisiteur['id'];
+			
 			$nom = $unVisiteur['nom'];
 			$prenom = $unVisiteur['prenom'];
             $adresse = $unVisiteur['adresse'];
             $cp = $unVisiteur['cp'];
             $ville = $unVisiteur['ville'];
             $date_embauche = $unVisiteur['dateEmbauche'];
-            $id_secteur = $unvisiteur['idSecteur'];            
+            $id_secteur = $unVisiteur['idSecteur'];            
 		?>
                 <tr>
-                    <td><?php echo $id ?></td>
+                    
                     <td><?php echo $nom ?></td>
                     <td><?php echo $prenom ?></td>
                     <td><?php echo $adresse ?></td>

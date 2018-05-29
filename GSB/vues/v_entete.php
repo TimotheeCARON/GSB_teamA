da3464656db436ea75168ec61b5791616cfdc339
Tomtheret <32103913+Tomtheret@users.noreply.github.com>
2018-05-29 15:25:37 +0200

    installation Bootstrap

--- GSB/vues/v_entete.php
+++ GSB/vues/v_entete.php
@@ -5,6 +5,9 @@
     <title>Intranet du Laboratoire Galaxy-Swiss Bourdin</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+    <link href="./styles/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="./styles/bootstrap.css" rel="stylesheet" type="text/css" />
+    <script type=”text/javascript” src=”./js/bootstrap.js”></script>
     <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico" />
   </head>
   <body>

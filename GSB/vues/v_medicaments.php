6c416dfb57f8838e1f7ebad11f1178cb627fd144
Sevone-1 <33868773+Sevone-1@users.noreply.github.com>
2018-06-04 23:15:15 +0200

    Adaptation V_Medicament au CSS
    Puting c bô

--- GSB/vues/v_medicaments.php
+++ GSB/vues/v_medicaments.php
@@ -22,166 +22,233 @@ else{
     $prix_echantillon = "";
 }
 ?>
-
-<div id="contenu">
-<?php 
-if ($_GET['action']=="UpdtMedoc"){
-    echo '<h2>Modifier un médicament</h2>
-    <form method="POST" action="index.php?uc=medicaments&action=ModifMedoc">';
-}
-else{
-    echo '<h2>Enregistrer un nouveau médicament</h2>
-    <form method="POST" action="index.php?uc=medicaments&action=NewMedoc">';
-}
-echo '  <input type="hidden" name="id" id="id" value="'.$id.'">
-        <input type="text" name="nomMedoc" id="nomMedoc" value="'.$nom.'">Nom du médicament<br />
-        <input type="text" name="Effet_therapeutique" id="Effet_therapeutique" value="'.$effet.'">Effet thérapeutique<br />
-        <input type="text" name="Contre_indication" id="Contre_indication" value="'.$contre_indication.'">Contre indication<br />
-        <input type="text" name="Presentation" id="Presentation" value="'.$presentation.'">Presentation<br />
-        <input type="text" name="Dosage" id="Dosage" value="'.$dosage.'">Dosage<br />
-        <input type="text" name="pxHT" id="pxHT" value="'.$prix_HT.'">pxHT<br />
-        <input type="text" name="pxEchantillon" id="pxEchantillon" value="'.$prix_echantillon.'">pxEchantillon<br />
-        <select name="famille">';
-?>
-            <?php
-                foreach ($lesFamillesMedicaments as $uneFamille){
-                    echo '<option value= '.$uneFamille['idFamille'].' > '.$uneFamille['nomFamille'].' </option>';
+<div class="text-center">
+    <div id="contenu">
+        <div id="contenu" class="container">
+            <h2> Medicaments </h2>
+            <div class="row centered-form">
+                
+                <div class="panel panel-default">
+                    
+                <?php 
+                if ($_GET['action']=="UpdtMedoc"){
+                    echo '<div class="panel-heading">
+                            <h3 class="panel-title">Modifier un médicament</h3>
+                        </div>
+                    <form method="POST" action="index.php?uc=medicaments&action=ModifMedoc">';
                 }
-            ?>
-            
-        </select> 
-        <?php
-        if (isset($_GET['UpdtId'])){
-            echo '<input type="submit" value="Modifier" />';
-        }
-        else{
-            echo '<input type="submit" value="Envoyer" />';
-        }
-        ?>
-    </form>
-</div>
-
-
-
-<div id="contenu">
-      <h2>Gérer les médicaments</h2>
-         
-      
-  	<table>
-  	   <caption>Descriptif des médicaments -
-           </caption>
-             <tr>
-                <th class="id">Numero</th>
-                <th class="nom">Nom</th>
-                <th class='famille'>Famille</th>  
-                <th class='effet'>Effet</th>
-                <th class='dosage'>Dosage</th>
-                <th class='presentation'>Présentation</th>
-                <th class='contre_indication'>Contre indication</th>
-                <th class='prix_HT'>Prix HT</th>
-                <th class='prix_echantillon'>Prix échantillon</th>
-                <th class='supprimer'>Supprimer</th>
+                else{
+                    echo '<div class="panel-heading">
+                            <h3 class="panel-title">Enregistrer un nouveau médicament</h3>
+                        </div>
+                    <form method="POST" action="index.php?uc=medicaments&action=NewMedoc">';
+                }
+                echo '  
+                        
+                        <input type="hidden" name="id" id="id" value="'.$id.'"> 
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control name="nomMedoc" id="nomMedoc" value="'.$nom.'" placeholder="Nom du médicament"><br />
+                                </div> 
+                            </div>
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control name="Effet_therapeutique" id="Effet_therapeutique" value="'.$effet.'" placeholder="Effet thérapeutique"><br />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control name="Contre_indication" id="Contre_indication" value="'.$contre_indication.'" placeholder="Contre indication"><br />
+                                </div>
+                            </div>
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control name="Presentation" id="Presentation" value="'.$presentation.'" placeholder="Presentation"><br />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control name="Dosage" id="Dosage" value="'.$dosage.'" placeholder="Dosage"><br />
+                                </div>
+                            </div>
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control name="pxHT" id="pxHT" value="'.$prix_HT.'" placeholder="pxHT"><br />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <div class="form-group">
+                                    <input type="text" class="form-control name="pxEchantillon" id="pxEchantillon" value="'.$prix_echantillon.'" placeholder="pxEchantillon"><br />
+                                </div>
+                            </div>
+                            <div class="col-xs-6 col-sm-6 col-md-6">
+                                <div class="form-group">
+                                    <select class="form-control" name="famille">
+                                ';
+                                
+                        ?>
+                        <?php
+                            foreach ($lesFamillesMedicaments as $uneFamille){
+                                echo '<option value= '.$uneFamille['idFamille'].' > '.$uneFamille['nomFamille'].' </option>';
+                            }
+                        ?>
+                                    </select> 
+                                </div>
+                            </div>
+                        </div>
+                        <?php
+                        if (isset($_GET['UpdtId'])){
+                            echo '<input type="submit" value="Modifier" class="btn btn-info btn-block"/> />';
+                        }
+                        else{
+                            echo '<input type="submit" value="Envoyer" class="btn btn-info btn-block"/>';
+                        }
+                        ?>
+                    </form>
                 
-             </tr>
-              <?php      
-          foreach ( $lesMedicaments as $unMedicament ) 
-		  {
-			$id = $unMedicament['id_produit'];
-			$nom = $unMedicament['Nom_commercial'];
-			$famille = $unMedicament['nomFamille'];
-            $effet = $unMedicament['Effet_therapeutique'];
-            $contre_indication = $unMedicament['Contre_indication'];
-            $presentation = $unMedicament['Presentation'];
-            $dosage = $unMedicament['Dosage'];
-            $prix_HT = $unMedicament['pxHT'];
-            $prix_echantillon = $unMedicament['pxEchantillon'];
-		?>
-                <tr>
-                    <td><?php echo $id ?></td>
-                    <td><?php echo $nom ?></td>
-                    <td><?php echo $famille ?></td>
-                    <td><?php echo $effet ?></td>
-                    <td><?php echo $dosage ?></td>
-                    <td><?php echo $presentation ?></td>
-                    <td><?php echo $contre_indication ?></td>
-                    <td><?php echo $prix_HT ?></td>
-                    <td><?php echo $prix_echantillon ?></td>
-                    <td><?php echo "<a href='index.php?uc=medicaments&action=SupprMedoc&id=$id' title='Supprimer'><img src='./images/Delete_icon.png' alt='Supprimer' /></a>
-                    <a href='index.php?uc=medicaments&action=UpdtMedoc&UpdtId=$id' title='Update'><img src='./images/Update_icon.png' alt='Update' /></a>";
+                </div>
+            </div>
+
+
+
+
+            <div id="contenu">   
+                <h2>Gérer les médicaments</h2>
+                
+                <table class="table table-striped custab">
+                <caption>Descriptif des médicaments -
+                    </caption>
+                        <tr>
+                            <th class="id">Numero</th>
+                            <th class="nom">Nom</th>
+                            <th class='famille'>Famille</th>  
+                            <th class='effet'>Effet</th>
+                            <th class='dosage'>Dosage</th>
+                            <th class='presentation'>Présentation</th>
+                            <th class='contre_indication'>Contre indication</th>
+                            <th class='prix_HT'>Prix HT</th>
+                            <th class='prix_echantillon'>Prix échantillon</th>
+                            <th class='supprimer'>Supprimer</th>
+                            
+                        </tr>
+                        <?php      
+                    foreach ( $lesMedicaments as $unMedicament ) 
+                    {
+                        $id = $unMedicament['id_produit'];
+                        $nom = $unMedicament['Nom_commercial'];
+                        $famille = $unMedicament['nomFamille'];
+                        $effet = $unMedicament['Effet_therapeutique'];
+                        $contre_indication = $unMedicament['Contre_indication'];
+                        $presentation = $unMedicament['Presentation'];
+                        $dosage = $unMedicament['Dosage'];
+                        $prix_HT = $unMedicament['pxHT'];
+                        $prix_echantillon = $unMedicament['pxEchantillon'];
                     ?>
-                    </td>
-                 </tr>
-                 
-                 <?php
-			}
-		?>
-    </table>
-</div>
-
-<div id="contenu">
-    <h2>Créer une interaction entre médicaments</h2>
-    <form method="POST" action="index.php?uc=medicaments&action=NewInterac">
-        <select name="interaction1">
-        <?php
-        foreach ( $lesMedicaments as $unMedicament ) 
-            {
-                $id = $unMedicament['id_produit'];
-                $nom = $unMedicament['Nom_commercial'];
-        ?>
-                
-                <?php
-                    echo '<option value= '.$unMedicament['id_produit'].' > '.$unMedicament['Nom_commercial'].' </option>';
-                
-            }
-        ?>
-        </select> 
-        <select name="interaction2">
-        <?php
-        foreach ( $lesMedicaments as $unMedicament ) 
-            {
-                $id = $unMedicament['id_produit'];
-                $nom = $unMedicament['Nom_commercial'];
-        ?>
-                
-                <?php
-                    echo '<option value= '.$unMedicament['id_produit'].' > '.$unMedicament['Nom_commercial'].' </option>';
-                
-            }
-        ?>
-        </select>
-        <input type="submit" value="Envoyer" /> 
-    </form>
-</div>
-
-
-<!-- A FAIRE SAC A VIANDE ! -->
-<div id="contenu">
-    <h2>Supprimer une interaction entre médicaments</h2>
-
-    <table>
-             <tr>
-                <th class="idInterac1">Le médicament   </th>
-                <th class="idInterac2">Intéragie avec</th>
-                
-             </tr>
-              <?php      
-          foreach ($lesInteractions as $uneInteraction ) 
-		  {
-			$id1 = $uneInteraction['id_produit'];
-			$id2 = $uneInteraction['id_produit_Medicament'];
-			
-		?>
-                <tr>
-                    <td><?php echo $id1 ?></td>
-                    <td><?php echo $id2 ?></td>                   
-                    <td><?php echo "<a href='index.php?uc=medicaments&action=SupprInteraction&id1=$id1&id2=$id2' title='Supprimer'><img src='./images/Delete_icon.png' alt='Supprimer' /></a>"
+                            <tr>
+                                <td><?php echo $id ?></td>
+                                <td><?php echo $nom ?></td>
+                                <td><?php echo $famille ?></td>
+                                <td><?php echo $effet ?></td>
+                                <td><?php echo $dosage ?></td>
+                                <td><?php echo $presentation ?></td>
+                                <td><?php echo $contre_indication ?></td>
+                                <td><?php echo $prix_HT ?></td>
+                                <td><?php echo $prix_echantillon ?></td>
+                                <td><?php echo "<a href='index.php?uc=medicaments&action=SupprMedoc&id=$id' title='Supprimer'><img src='./images/Delete_icon.png' alt='Supprimer' /></a>
+                                <a href='index.php?uc=medicaments&action=UpdtMedoc&UpdtId=$id' title='Update'><img src='./images/Update_icon.png' alt='Update' /></a>";
+                                ?>
+                                </td>
+                            </tr>
+                            
+                            <?php
+                        }
                     ?>
-                    </td>
-                 </tr>
-                 
-                 <?php
-			}
-		?>
-    </table>
-</div>
+                </table>
+            </div>
 
+            <div id="contenu">
+                <h2>Créer une interaction entre médicaments</h2>
+                <form method="POST" action="index.php?uc=medicaments&action=NewInterac">
+                    <div class="row">
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <select class="form-control" name="interaction1">
+                            <?php
+                            foreach ( $lesMedicaments as $unMedicament ) 
+                                {
+                                    $id = $unMedicament['id_produit'];
+                                    $nom = $unMedicament['Nom_commercial'];
+                            ?>
+                                    
+                                    <?php
+                                        echo '<option value= '.$unMedicament['id_produit'].' > '.$unMedicament['Nom_commercial'].' </option>';
+                                    
+                                }
+                            ?>
+                                </select> 
+                            </div>
+                        </div>
+                        <div class="col-xs-6 col-sm-6 col-md-6">
+                            <div class="form-group">
+                                <select class="form-control" name="interaction2">
+                                <?php
+                                foreach ( $lesMedicaments as $unMedicament ) 
+                                    {
+                                        $id = $unMedicament['id_produit'];
+                                        $nom = $unMedicament['Nom_commercial'];
+                                ?>
+                                        
+                                        <?php
+                                            echo '<option value= '.$unMedicament['id_produit'].' > '.$unMedicament['Nom_commercial'].' </option>';
+                                        
+                                    }
+                                ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <input type="submit" value="Envoyer" class="btn btn-info btn-block" /> 
+                    
+                </form>
+            </div>
+
+
+            <div id="contenu">
+                <h2>Supprimer une interaction entre médicaments</h2>
+
+                <table class="table table-striped custab">
+                        <tr>
+                            <th class="idInterac1">Le médicament   </th>
+                            <th class="idInterac2">Intéragie avec</th>
+                            
+                        </tr>
+                        <?php      
+                    foreach ($lesInteractions as $uneInteraction ) 
+                    {
+                        $id1 = $uneInteraction['id_produit'];
+                        $id2 = $uneInteraction['id_produit_Medicament'];
+                        
+                    ?>
+                            <tr>
+                                <td><?php echo $id1 ?></td>
+                                <td><?php echo $id2 ?></td>                   
+                                <td><?php echo "<a href='index.php?uc=medicaments&action=SupprInteraction&id1=$id1&id2=$id2' title='Supprimer'><img src='./images/Delete_icon.png' alt='Supprimer' /></a>"
+                                ?>
+                                </td>
+                            </tr>
+                            
+                            <?php
+                        }
+                    ?>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
